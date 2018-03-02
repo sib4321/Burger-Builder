5ffdcd623f1ed525d4d3f67b9b2a79a8a1cf7b52
sib4321 <sib4321@ukr.net>
2018-03-02 16:15:38 +0200

    Add BurgerBuilder with modal

---
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import Auxil from '../../../hoc/Auxil';
+import Button from '../../UI/Button/Button';
+
+const orderSummary = (props) => {
+  const ingredientSummary = Object.keys(props.ingredients)
+    .map(igKey => {
+      return  (
+        <li key={igKey}>
+          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+        </li>);
+    });
+
+  return (
+    <Auxil>
+      <h3>Your Order</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>
+        {ingredientSummary}
+      </ul>
+      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+      <p>Continue to Checkout?</p>
+      <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+      <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+    </Auxil>
+  );
+};
+
+export default orderSummary;
