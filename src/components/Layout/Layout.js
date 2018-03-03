77fed71ef25b2d77ba9668b0155640c0fad6dfb4
sib4321 <sib4321@ukr.net>
2018-03-03 14:19:15 +0200

    Add the menu and responsiveness

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Auxil from '../../hoc/Auxil';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-  <Auxil>
-    <div>Toolbar, SideDrawer, Backdrop</div>
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Auxil>
-);
+class Layout  extends Component {
+  state = {
+    showSideDrawer: false
+  }
 
-export default layout;
+  sideDrawerClosedHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
+
+  sideDrawerToggleHandler = () => {
+    this.setState(( prevState ) => {
+      return { showSideDrawer: !prevState.showSideDrawer }
+    });
+  }
+
+  render () {
+    return (
+      <Auxil>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler }/>
+        <SideDrawer open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler} />
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Auxil>
+    )
+  }
+}
+
+export default Layout;
