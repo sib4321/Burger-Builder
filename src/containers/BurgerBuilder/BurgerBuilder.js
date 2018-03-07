e7b0d4ab7ca0bfc42e54c679f006d4aa5b408916
sib4321 <sib4321@ukr.net>
2018-03-07 18:09:50 +0200

    Add routing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -31,6 +31,7 @@ class BurgerBuilder extends Component {
   }
 
   componentDidMount () {
+    console.log(this.props);
     axios.get('https://burger-builder-be96d.firebaseio.com/ingredients.json')
       .then(response => {
           this.setState({ingredients: response.data});
@@ -93,29 +94,18 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert('You continue!');
-    this.setState( { loading: true } );
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: 'Serge Bubelo',
-        address: {
-          street: 'Teststreet 1',
-          zipCode: '12345',
-          country: 'Ukraine'
-        },
-        email : 'test@test.com'
-      },
-      deliveryMethod: 'fastest'
+
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
     }
-    axios.post('/orders.json', order)
-      .then(response => {
-        this.setState({ loading: false, purchasing: false });
-      } )
-      .catch(error => {
-        this.setState({ loading: false, purchasing: false });
-      });
-  }
+    queryParams.push('price=' + this.state.totalPrice);
+    const queryString = queryParams.join('&');
+    this.props.history.push({
+      pathname: '/checkout',
+      search: '?' + queryString
+    });
+   }
 
   render () {
     const disabledInfo = {
