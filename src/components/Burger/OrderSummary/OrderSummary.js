1d0b7584ff9d93f91b423edefcf88f74bc224c60
sib4321 <sib4321@ukr.net>
2018-03-16 12:42:50 +0200

    Optimize the application

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,10 +4,6 @@ import Auxil from '../../../hoc/Auxil/Auxil';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
-    // This could be a functional component, doesn't have to be a class
-    componentWillUpdate() {
-        console.log('[OrderSummary] WillUpdate');
-    }
 
     render () {
         const ingredientSummary = Object.keys( this.props.ingredients )
